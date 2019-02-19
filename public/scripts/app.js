a88a8acec7f8f47e614b3c57af9155961ca8f36f
sddanila <sddanila@yahoo.co.uk>
2019-02-19 23:58:28 +0000

    adding app.js that appends a new tweet with generic text

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,101 +4,108 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-}
+// const data = {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": {
+//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//       },
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+// }
 
-function createTweetElement(tweetObject) {
+
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+
+function createTweetElement(data) {
     console.log('This is working!');
 
-    let $tweet = $('<article>').addClass('tweet');
-    
-    
-    return $tweet;
+    let username = data.user.name;
+    let avatar = data.user.avatars.small;
+    let userId = data.user.handle;
+    let tweetText = data.content.text;
+    let time = data.created_at;
 
-
-
-    const tweetForm = document.querySelector('#tweet-form');
-
-    tweetForm.addEventListener('submit', function(event){
-        event.preventDefault();
-        // create new section
-        const container = document.querySelector('.container');
-        const newSection = document.createElement('section');
-        container.append(newSection);
-
-        // create new article
-        const newArticle = document.createElement('article')
-        const section = document.querySelector('.new-tweet')
-        section.append(newArticle);
-
-        // create new header
-        const newHeader = document.createElement('header');
-        const article = document.querySelector('.tweet');
-        article.append(newHeader);
-
-        // create new h3
-        const newH3 = document.createElement('h3');
-        const header = document.querySelector('.new-tweet-header');
-        header.append(newH3);
-        
-        // create new img
-        const newAvatar = document.createElement('img');
-        const image = tweetObject.user.avatars.small;
-        newAvatar.innerText = image;
-        const headerText = document.querySelector('#new-tweet-text').value;
-        const h3 = document.querySelector('.name-h3');
-        h3.append(newAvatar);
-        h3.innerText = headerText;
-        
-        // create new span
-        const newSpan = document.createElement('span');
-        const username = tweetObject.user.handle;
-        newSpan.innerText = username;
-        h3.append(newSpan);
-
-        // create new paragraph
-        const newParagraph = document.createElement('paragraph');
-        const tweetText = tweetObject.content;
-        newParagraph.innerText = tweetText;
-        article.append(newParagraph);
-
-        // create new footer
-        const newFooter = document.createElement('footer');
-        article.append(newFooter);
-
-        // create new div for time
-        const newDiv = document.createElement('div')
-        const time = tweetObject[created_at];
-        newDiv.innerText = time;
-        const footer = document.querySelector('.tweet-footer');
-        footer.append(newDiv);
-
-        // create new div for social media icons
-        const newDivIcons = document.createElement('div');
-        footer.append(newDivIcons);
-    })
-};
-
-var $tweet = createTweetElement(tweetData);
-$(document).ready(console.log($tweet));
-// $('#tweets-container').append($tweet);
-
-function renderTweets(data){
-    for (user in data){
-        let newTweet = createTweetElement(user);
-        $('#tweets-container').append($newTweet);
-    }
+    $(".tweets-container").append(`
+    <section id="container">
+        <article class="tweet">
+          <header class="new-tweet-header">
+            <h3 class="name-h3">
+              <img class="userIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png">
+              Name Name Name
+            </h3>
+            <span class="userId">@Name</span>
+          </header>
+            <p>Saying hi to all my tweety friends!</p>
+          <footer class="tweet-footer">
+            <div>13 days ago</div>
+            <div class="sn-icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i><div>
+          </footer>
+        </article>
+      </section>
+    `);
+    // return $tweet;
 }
+
+// function renderTweets(data){
+//     for (var i = 0; i < data.length; i++){
+//         let $newTweet = createTweetElement(data[i]);
+//         $('#tweets-container').append($newTweet);
+//     }
+// }
+
+$(document).ready(createTweetElement(data));
+
+
