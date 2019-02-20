bb1b8a6e0fd4835c7311ed27ae414c2ad38175c1
sddanila <sddanila@yahoo.co.uk>
2019-02-20 00:18:40 +0000

    adding app.js that appends all data from the data array of users

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -22,51 +22,51 @@
 
 
 const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+{
+    "user": {
+    "name": "Newton",
+    "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+    "handle": "@SirIsaac"
     },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
+    "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+},
+{
+    "user": {
+    "name": "Descartes",
+    "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+    },
+    "handle": "@rd" },
+    "content": {
+    "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+},
+{
+    "user": {
+    "name": "Johann von Goethe",
+    "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+    },
+    "handle": "@johann49"
+    },
+    "content": {
+    "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+}
+];
 
 
 function createTweetElement(data) {
@@ -78,34 +78,34 @@ function createTweetElement(data) {
     let tweetText = data.content.text;
     let time = data.created_at;
 
-    $(".tweets-container").append(`
+    let $tweet = $(".tweets-container").append(`
     <section id="container">
         <article class="tweet">
           <header class="new-tweet-header">
             <h3 class="name-h3">
-              <img class="userIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png">
-              Name Name Name
+              <img class="userIcon" src=${avatar}>
+              ${username}
             </h3>
-            <span class="userId">@Name</span>
+            <span class="userId">${userId}</span>
           </header>
-            <p>Saying hi to all my tweety friends!</p>
+            <p>${tweetText}</p>
           <footer class="tweet-footer">
-            <div>13 days ago</div>
+            <div>${time}</div>
             <div class="sn-icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i><div>
           </footer>
         </article>
       </section>
     `);
-    // return $tweet;
+    return $tweet;
 }
 
-// function renderTweets(data){
-//     for (var i = 0; i < data.length; i++){
-//         let $newTweet = createTweetElement(data[i]);
-//         $('#tweets-container').append($newTweet);
-//     }
-// }
+function renderTweets(data){
+    for (var i = 0; i < data.length; i++){
+        let $newTweet = createTweetElement(data[i]);
+        $('#tweets-container').append($newTweet);
+    }
+}
 
-$(document).ready(createTweetElement(data));
+$(document).ready(renderTweets(data));
 
 
