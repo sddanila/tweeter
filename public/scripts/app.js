38fd10158e98bad1e355aaa04f4f1761f319f93d
sddanila <sddanila@yahoo.co.uk>
2019-02-20 21:43:27 +0000

    fixed issue of tweets appending instead of prepending - now they are going to the top and the class-system has been updated

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,11 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+}
 
 function createTweetElement(data) {
     console.log('This is working!');
@@ -10,34 +15,34 @@ function createTweetElement(data) {
     let username = data.user.name;
     let avatar = data.user.avatars.small;
     let userId = data.user.handle;
-    let tweetText = data.content.text;
+    let tweetText = escape(data.content.text);
     let time = data.created_at;
 
-    let $tweet = $(".tweets-container").append(`
+    let $tweet = `
         <section id="container">
             <article class="tweet">
-            <header class="new-tweet-header">
-                <h3 class="name-h3">
+            <header>
+                <h3>
                 <img class="userIcon" src=${avatar}>
                 ${username}
                 </h3>
-                <span class="userId">${userId}</span>
+                <span>${userId}</span>
             </header>
             <p>${tweetText}</p>
-            <footer class="tweet-footer">
+            <footer>
                 <div>${time}</div>
                 <div class="sn-icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i><div>
             </footer>
             </article>
         </section>
-    `);
+    `;
     return $tweet;
 }
 
 function renderTweets(data){
     for (var i = 0; i < data.length; i++){
         let $newTweet = createTweetElement(data[i]);
-        $('#tweets-container').append($newTweet);
+        $('.tweets-container').append($newTweet);
     }
 }
 
@@ -46,7 +51,6 @@ $(document).ready($(function() {
     $form.on('submit', function (event) {
         event.preventDefault();
         let tweetText = $('#textarea').val();
-        console.log(tweetText.length);
         if( tweetText.length > 140 || tweetText.length <= 0){
             alert("Your tweet is too long or too short!");
         } else {
@@ -56,20 +60,20 @@ $(document).ready($(function() {
                 method: 'POST',
                 data: { text: tweetText },
                 success: function (data) {
-                    $('#tweets-container').append(createTweetElement(data);
-                    console.log(data);
+                    $('.new-tweet').after(createTweetElement(data));
+                    $('#container').text
                 }
             })
         }
     });
-
+// GET EXISTING TWEETS FROM DATABASE
     $.getJSON({
         url: 'http://localhost:8080/tweets',
         method: 'GET',
         data: {get_param: 'value'},
         success: function (data) {
             renderTweets(data);
-            console.log('First Success!');
+            console.log('Success loading original database!');
         }
     });
 }))
